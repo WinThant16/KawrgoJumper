9acf8232e42eb946f952e97b6f64b7230b98d2a6
karyeet <ksheh002@ucr.edu>
2024-12-08 10:29:45 -0800

    uploadmanifest, getcurrentmanifest requestlib functions

--- src/lib/requestLib.js
+++ src/lib/requestLib.js
@@ -2,7 +2,6 @@ import axios from "axios";
 const HOST = "http://localhost:8080"
 
 
-
 // log text is a string
 function submitLog(logText){
     axios.post(`${HOST}/api/submitLogEntry`, logText, {headers: { "Content-Type": "text/plain; charset=ascii"}}).then((res)=>{
@@ -13,7 +12,22 @@ function submitLog(logText){
     })
 }
 
-const reqLib = {submitLog}
+function uploadManifest(name, contents){
+  axios.post(`${HOST}/api/uploadManifest`, {"fileName":name, "fileContents":contents}, {headers: { "Content-Type": "application/json; charset=ascii"}}).then((res)=>{
+    console.log("Uploaded manifest with response " + res.data);
+  })
+  .catch(error =>{
+    console.error(error);
+  })
+}
 
-export {submitLog};
+// promise, must use .then((manifest)=>{})
+async function getCurrentManifest(){
+  const res = await axios.get(`${HOST}/api/getCurrentManifest`);
+  return res.data;
+}
+
+const reqLib = {submitLog, uploadManifest, getCurrentManifest}
+
+export {submitLog, uploadManifest, getCurrentManifest};
 export default reqLib;
