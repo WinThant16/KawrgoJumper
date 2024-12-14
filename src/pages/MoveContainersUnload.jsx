7eebad725afa7d6e88a28b8864132c90fc854e9a
karyeet <ksheh002@ucr.edu>
2024-12-13 22:36:36 -0800

    goes step by step, no manifest update

--- src/pages/MoveContainersUnload.jsx
+++ src/pages/MoveContainersUnload.jsx
@@ -10,28 +10,44 @@ import { shallow_extended_matrix } from "../lib/taskcommon";
 
 
 function MoveContainersUnload(){
-  const [gridData, setGridData] = useState([]);
-  const [selected_containers, setSelectedContainers] = useState([]);
+  const [stepi, setStep] = useState(0);
   const [hoveredContainer, setHoveredContainer] = useState({ name: "", weight: "", row: 0, col: 0 });
   const currentFile = localStorage.getItem("manifestFileName");
   const jobType = localStorage.getItem("jobType");
   const manifest_matrix = shallow_extended_matrix(parse_manifest(localStorage.getItem("manifestFileContent"), []));
   const navigate = useNavigate();
 
-  //localStorage.setItem("currentPage", "move-containers-unload");
-  console.log("steps", localStorage.getItem("steps"))
+  let destination_container;// = [steps[0].path[steps[0].path.length-1]];
+  let start_container;// = [steps[0].source];
+  let path_containers;// = steps[0].path;
   const steps = JSON.parse(localStorage.getItem("steps")); //{"destination": [1,2], "start": [3,4], "path":[[1,2],[3,4]]} //localStorage.getItem("steps")
-  console.log(steps[0])
-  console.log(steps[0].path, steps[0].path.length-1)
-  console.log([steps[0].path[steps[0].path.length-1]])
-  const destination_container = [steps[0].path[steps[0].path.length-1]];
-  const start_container = [steps[0].source];
-  const path_containers = steps[0].path;
+
+  //localStorage.setItem("currentPage", "move-containers-unload");
+  //console.log("steps", localStorage.getItem("steps"))
+  
+  //console.log(steps[0])
+ // console.log(steps[0].path, steps[0].path.length-1)
+  //console.log([steps[0].path[steps[0].path.length-1]])
+  console.log("steps", steps);
+  console.log("outside processstep", stepi)
+  const processStep = () =>{
+    console.log("inside processstep", stepi)
+    console.log("AAA", steps[stepi])
+    console.log("BBB", steps[stepi].path, steps[stepi].path.length-1)
+    console.log("CCC", [steps[stepi].path[steps[stepi].path.length-1]])
+    destination_container = [steps[stepi].path[steps[stepi].path.length-1]]
+    start_container = [steps[stepi].source];
+    path_containers = steps[stepi].path;
+  }
 
   const nextStep = () => {
-    navigate("/");
+    setStep(stepi+1);
+    processStep();
+    //navigate("/");
   };
 
+  processStep();
+  
   return (
     <div className="select-containers-page">
       <Navbar />
